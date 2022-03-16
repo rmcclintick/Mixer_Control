fc33c92ba86006ef4a5708534500fa21a4d0e6cc
rmcclintick <robert.mcc3@gmail.com>
2022-03-15 20:23:02 -0700

    early networking

--- UDPJavaServer.py
+++ UDPJavaServer.py
@@ -3,10 +3,12 @@ import socket
 import threading
 import socketserver
 import time
+from ui import MixerControl
 
 JAVA_SERVER_HOST = "192.168.1.245"
 JAVA_SERVER_PORT = 4445
 PYTHON_SERVER_PORT = 4446
+mixer = None
 
 def sendMessage(message):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -16,33 +18,46 @@ def sendMessage(message):
     finally:
         sock.close()
 
+def dispose():
+    server.shutdown()
+    server.server_close()
+
 # Put the code you want to do something based on when you get data here.
 def onData(data):
-    print("Python got data: " + data.decode("utf-8"), end = "\n")
+    message = data.decode("utf-8")
+    if message == "refresh":
+        mixer.refreshSessions(mixer)
+    else:
+        mixer.updateMixer(mixer, int(message), 0)
+    
     
 class ThreadedUDPRequestHandler(socketserver.BaseRequestHandler):
+    print("thread")
     def handle(self):
         data = self.request[0].strip()
         socket = self.request[1]
         onData(data)
 
 class ThreadedUDPServer(socketserver.ThreadingMixIn, socketserver.UDPServer):
-    pass
+    global mixer
+    mixer = 
+    print(mixer.getLength())
+    #pass
 
 # This starts up the server, sends a few messages and then shuts the server down.
-server = ThreadedUDPServer(("192.168.1.198", PYTHON_SERVER_PORT), ThreadedUDPRequestHandler)
-server_thread = threading.Thread(target=server.serve_forever)
-server_thread.daemon = True
-server_thread.start()
-print("Server loop running in thread: ", server_thread.name)
-time.sleep(3)
+##server = ThreadedUDPServer(("192.168.1.198", PYTHON_SERVER_PORT), ThreadedUDPRequestHandler)
+##server_thread = threading.Thread(target=server.serve_forever)
+##server_thread.daemon = True
+##server_thread.start()
+##print("Server loop running in thread: ", server_thread.name)
+#time.sleep(3)
 #sendMessage("Hello Java 1")
-time.sleep(3)
+#time.sleep(3)
 #sendMessage("Hello Java 2")
-time.sleep(3)
+#time.sleep(3)
 #sendMessage("Hello Java 3")
-time.sleep(5)
+#time.sleep(5)
 
-print("\nShutting down Python server")
-server.shutdown()
-server.server_close()
+#time.sleep(60)
+##print("\nShutting down Python server")
+
--- ui.py
+++ ui.py
@@ -2,82 +2,133 @@
 from tkinter import *
 from audio_controller import *
 from pycaw.pycaw import AudioUtilities
-  
+from UDPJavaServer import *
+
+
+LOCAL_IP = "192.168.1.198"
+PYTHON_SERVER_PORT = 4446
 
 
 names = []
 faders = []
 audioControllers = []
+root = None
+server = None
+server_thread = None
 
-def createFaders():
-    global names
-    global faders
-    global audioControllers
-    sessions = AudioUtilities.GetAllSessions()
-    for i in sessions:
-        if hasattr(i.Process, 'name'):
-            names.append(i.Process.name())
-    names = list(dict.fromkeys(names))
-
-    for i in range(len(names)):
-        ac = AudioController(names[i])
-        audioControllers.append(ac)
-        faders.append(Scale(root, from_ = 0, to = 1, resolution = 0.01,
-                            orient = HORIZONTAL, command = updateMixer,
-                            label = names[i], length = 200))
-        faders[i].set(ac.process_volume())
-    for fader in faders:
-        fader.pack()
-
-def createRefreshBtn():
-    b2 = Button(root, text ="Refresh",
-            command = refreshSessions,
-            padx = 10, pady = 10)
-    b2.pack()
-
-
-def refreshSessions():
-    global names
-    global faders
-    global audioControllers
-    sessions = AudioUtilities.GetAllSessions()
+class MixerControl:
     
-    names.clear()
-    for i in sessions:
-        if hasattr(i.Process, 'name'):
-            names.append(i.Process.name())
-    names = list(dict.fromkeys(names))
+    def updateMixers(self, val):
+        global audioControllers
+        for i in range(len(faders)):
+            audioControllers[i].set_volume(faders[i].get())
 
-    for fader in faders:
-        fader.destroy()
-    faders.clear()
-    audioControllers.clear()
-    for i in range(len(names)):
-        ac = AudioController(names[i])
-        audioControllers.append(ac)
-        faders.append(Scale(root, from_ = 0, to = 1, resolution = 0.01,
-                            orient = HORIZONTAL, command = updateMixer,
-                            label = names[i], length = 200))
-        faders[i].set(ac.process_volume())
-    for fader in faders:
-        fader.pack()
+    def updateMixer(self, val, i):
+        global audioControllers
+        global faders
+        print(i)
+        print(len(faders))
+        print(len(audioControllers))
+        audioControllers[i].set_volume(val / 100)
 
-def updateMixer(val):
-    for i in range(len(faders)):
-        audioControllers[i].set_volume(faders[i].get())
+            
+    def createFaders(self):
+        global names
+        global faders
+        global audioControllers
+        global root
+        sessions = AudioUtilities.GetAllSessions()
+        for i in sessions:
+            if hasattr(i.Process, 'name'):
+                names.append(i.Process.name())
+        names = list(dict.fromkeys(names))
 
-def getProgNameList():
-    global names
-    return names
+        for i in range(len(names)):
+            global audioController
+            ac = AudioController(names[i])
+            audioControllers.append(ac)
+            faders.append(Scale(root, from_ = 0, to = 1, resolution = 0.01,
+                                orient = HORIZONTAL, command = self.updateMixers,
+                                label = names[i], length = 200))
+            faders[i].set(ac.process_volume())
+        for fader in faders:
+            fader.pack()
+        print("AC length :" + str(len(audioControllers)))
+
+    #debug only
+    def printMessage(message):
+        print(message)
+    def getLength():
+        global audioControllers
+        return len(audioControllers)
+
+    def refreshSessions(self):
+        global names
+        global faders
+        global audioControllers
+        global root
+        sessions = AudioUtilities.GetAllSessions()
+        
+        names.clear()
+        for i in sessions:
+            if hasattr(i.Process, 'name'):
+                names.append(i.Process.name())
+        names = list(dict.fromkeys(names))
+
+        for fader in faders:
+            fader.destroy()
+        faders.clear()
+        audioControllers.clear()
+        for i in range(len(names)):
+            ac = AudioController(names[i])
+            audioControllers.append(ac)
+            faders.append(Scale(root, from_ = 0, to = 1, resolution = 0.01,
+                                orient = HORIZONTAL, command = self.updateMixers,
+                                label = names[i], length = 200))
+            faders[i].set(ac.process_volume())
+        for fader in faders:
+            fader.pack()
+
+            
+    def createRefreshBtn(self):
+        global root
+        b2 = Button(root, text ="Refresh",
+                command = self.refreshSessions,
+                padx = 10, pady = 10)
+        b2.pack()
 
 
-root = Tk()
-root.title('Mixer Control')
-root.geometry("250x300") 
-createRefreshBtn()
-createFaders()
+    def getProgNameList():
+        global names
+        return names
 
-#l2 = Label(root)
+    def onClosing(self):
+        global server
+        server.shutdown()
+        server.server_close()
 
-#l2.pack()
-root.mainloop()
+    def __init__(self):
+        global root
+        global server
+        global server_thread
+        root = Tk()
+        root.title('Mixer Control')
+        root.geometry("250x300")
+        self.createRefreshBtn()
+        self.createFaders()
+        #root.protocol("WM_DELETE_WINDOW", self.onClosing)
+        root.mainloop()
+
+        
+
+def main():
+    ui = MixerControl()
+    server = ThreadedUDPServer((LOCAL_IP, PYTHON_SERVER_PORT), ThreadedUDPRequestHandler, ui)
+    server_thread = threading.Thread(target=server.serve_forever)
+    server_thread.daemon = True
+    server_thread.start()
+    
+
+if __name__ == "__main__":
+    main()
+
